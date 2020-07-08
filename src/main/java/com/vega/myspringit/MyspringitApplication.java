d798248538207de05c24cc67625de102ce01a718
jmcaplan <54451212+jmcaplan@users.noreply.github.com>
2020-07-08 16:08:30 -0400

    first commit

---
+++ src/main/java/com/vega/myspringit/MyspringitApplication.java
@@ -0,0 +1,14 @@
+package com.vega.myspringit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringitApplication.class, args);
+		System.out.println("Hello to my springit");
+	}
+
+}
---
+++ src/test/java/com/vega/myspringit/MyspringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vega.myspringit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
